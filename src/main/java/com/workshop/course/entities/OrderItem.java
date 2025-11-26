0ea6c7b88c1c8fb21870e3cbd58c480714a570db
VinniGSBP <gs.vinni@gmail.com>
2025-11-26 18:51:38 -0300

    OrderItem, ManyToMany assosciation with extra attributes pt2

--- src/main/java/com/workshop/course/entities/OrderItem.java
+++ src/main/java/com/workshop/course/entities/OrderItem.java
@@ -1,8 +1,8 @@
 package com.workshop.course.entities;
 
 import java.io.Serializable;
-import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.workshop.course.entities.pk.OrderItemPK;
 
 import jakarta.persistence.EmbeddedId;
@@ -15,9 +15,8 @@ public class OrderItem implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@EmbeddedId
-	private OrderItemPK id;
-	
-	
+	private OrderItemPK id = new OrderItemPK();
+
 	private Integer quantity;
 	private Double price;
 
@@ -30,9 +29,9 @@ public class OrderItem implements Serializable {
 		id.setProduct(product);
 		this.quantity = quantity;
 		this.price = price;
-
 	}
 
+	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
@@ -67,7 +66,10 @@ public class OrderItem implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -79,7 +81,11 @@ public class OrderItem implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		OrderItem other = (OrderItem) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-
 }
